b83980eabd409948451764d11b359cf0f94fdc95
Ladislav Margai <LadislavM@kentico.com>
2016-03-09 23:21:55 +0100

    Solution for problem #3

---
+++ ProjectEuler/Problem003.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ProjectEulerTests
+{
+    [TestFixture]
+    public class Problem003
+    {
+        public IEnumerable<int> GetPrimeFactorsOfNumber(long number)
+        {
+            long sum = 1;
+
+            for (var i = 2; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum *= i;
+
+                    if (sum <= number)
+                    {
+                        yield return i;
+                    }
+                    else
+                    {
+                        break; 
+                    }
+                }
+            }
+        }
+
+
+        [TestCase(13195, new[] { 5, 7, 13, 29 })]
+        public void GetPrimeFactorsOfNumber_Test(int number, IEnumerable<int> expectedResult)
+        {
+            Assert.AreEqual(expectedResult, GetPrimeFactorsOfNumber(number));
+        }
+
+
+        [TestCase(13195, 29)]
+        [TestCase(600851475143, 6857)]
+        public void GetLargestPrimeFactorOfNumber_Test(long number, int expectedLargestPrimeFactor)
+        {
+            Assert.AreEqual(expectedLargestPrimeFactor, GetPrimeFactorsOfNumber(number).Last());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ My solutions of problems from [projecteuler.net](https://projecteuler.net/) in C
 
 ### Problems 1-100	
 Problem #1: [Multiples of 3 and 5](https://projecteuler.net/problem=1) [[Solution](https://github.com/LadislavMargai/ProjectEuler/blob/master/ProjectEuler/Problem001.cs)]
+Problem #3: [Largest prime factor](https://projecteuler.net/problem=3) [[Solution](https://github.com/LadislavMargai/ProjectEuler/blob/master/ProjectEuler/Problem003.cs)]
