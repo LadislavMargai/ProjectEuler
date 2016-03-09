46ef4283a498358deb7efefe6797c841a80ab383
Ladislav Margai <LadislavM@kentico.com>
2016-03-09 22:29:17 +0100

    Solution for problem #1

---
+++ ProjectEuler/Problem001.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ProjectEulerTests
+{
+    [TestFixture]
+    public class Problem001
+    {
+        public IEnumerable<int> GetNaturalNumbersWhichAreMultiplesOf3Or5(int maxValue, int start = 1)
+        {
+            for (var i = start; i < maxValue; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+
+        [TestCase(10, new[] { 3, 5, 6, 9 })]
+        public void GetNaturalNumbersWhichAreMultiplesOf3Or5_Test(int maxValue, IEnumerable<int> expectedResult)
+        {
+            Assert.AreEqual(expectedResult, GetNaturalNumbersWhichAreMultiplesOf3Or5(maxValue));
+        }
+
+
+        [TestCase(10, 23)]
+        [TestCase(1000, 233168)]
+        public void GetSumOfNaturalNumbersWhichAreMultiplesOf3Or5_Test(int maxValue, int expectedSum)
+        {
+            Assert.AreEqual(expectedSum, GetNaturalNumbersWhichAreMultiplesOf3Or5(maxValue).Sum());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ![Test status](http://flauschig.ch/batch.php?type=tests&account=LadislavMargai&slug=ProjectEuler)
 
 My solutions of problems from projecteuler.net in C#
+
+### Problems 1-100	
+Problem #1: [Multiples of 3 and 5](https://projecteuler.net/problem=1) [[Solution](https://github.com/LadislavMargai/ProjectEuler/blob/master/ProjectEuler/Problem001.cs)]
